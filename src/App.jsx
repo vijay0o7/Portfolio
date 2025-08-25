c209a3f2d4966e7946622d3fbfcfb33e7f831c95
vijay <vijaysankar2k4@gmail.com>
2025-08-25 13:35:02 +0530

    dvz

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
@@ -8,30 +8,42 @@ import Navbar from "./components/Navbar";
 import Portofolio from "./Pages/Portofolio";
 import ContactPage from "./Pages/Contact";
 import ProjectDetails from "./components/ProjectDetail";
+import WelcomeScreen from "./Pages/WelcomeScreen";
+import { AnimatePresence } from "framer-motion";
+import notfound from "./Pages/404";
 import NotFoundPage from "./Pages/404";
-import { AnimatePresence } from "framer-motion"; // (optional, can remove if unused)
 
-const LandingPage = () => {
+const LandingPage = ({ showWelcome, setShowWelcome }) => {
   return (
     <>
-      <Navbar />
-      <AnimatedBackground />
-      <Home />
-      <About />
-      <Portofolio />
-      <ContactPage />
-      <footer>
-        <center>
-          <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
-          <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
-            © 2025{" "}
-            <a href="https://flowbite.com/" className="hover:underline">
-              Vijay™
-            </a>
-            . All Rights Reserved.
-          </span>
-        </center>
-      </footer>
+      <AnimatePresence mode="wait">
+        {showWelcome && (
+          <WelcomeScreen onLoadingComplete={() => setShowWelcome(false)} />
+        )}
+      </AnimatePresence>
+
+      {!showWelcome && (
+        <>
+          <Navbar />
+          <AnimatedBackground />
+          <Home />
+          <About />
+          <Portofolio />
+          <ContactPage />
+          <footer>
+            <center>
+              <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
+              <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
+                © 2025{" "}
+                <a href="https://flowbite.com/" className="hover:underline">
+                  Vijay™
+                </a>
+                . All Rights Reserved.
+              </span>
+            </center>
+          </footer>
+        </>
+      )}
     </>
   );
 };
@@ -55,12 +67,22 @@ const ProjectPageLayout = () => (
 );
 
 function App() {
+  const [showWelcome, setShowWelcome] = useState(true);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route
+          path="/"
+          element={
+            <LandingPage
+              showWelcome={showWelcome}
+              setShowWelcome={setShowWelcome}
+            />
+          }
+        />
         <Route path="/project/:id" element={<ProjectPageLayout />} />
-        <Route path="*" element={<NotFoundPage />} /> {/* 404 route */}
+        <Route path="*" element={<NotFoundPage />} /> {/* Ini route 404 */}
       </Routes>
     </BrowserRouter>
   );
